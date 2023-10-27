e1c4080c6f0d7ad10f9a4135c957a8c58e32b3b7
Tristan <tristanpang@users.noreply.github.com>
2023-10-27 12:05:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-import GUI_file_importer
-import data_clean
-import plotting
+import Point_XYZ_Package as xyz
 
 def main():
   # GUI import
-  collar_data, survey_data, sample_data, geology_data = GUI_file_importer.main()
+  collar_data, survey_data, sample_data, geology_data = xyz.main()
 
   # Data stuff
-  surveys = data_clean.import_survey(survey_data)
-  collars = data_clean.import_collar(collar_data, surveys)  
-  samples = data_clean.import_sample(sample_data, collars)
-  tops = data_clean.import_geology(geology_data, collars)
+  surveys = xyz.import_survey(survey_data)
+  collars = xyz.import_collar(collar_data, surveys)  
+  samples = xyz.import_sample(sample_data, collars)
+  tops = xyz.import_geology(geology_data, collars)
 
   # Plotting
-  plotting.plot(collars, tops)
+  xyz.plot(collars, tops)
   # Saving
   print(collars, surveys, samples, tops)
   return collars, surveys, samples, tops
