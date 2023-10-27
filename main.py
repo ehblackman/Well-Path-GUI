e2af5545f8b6fde7451025ac98a1cd2da837df14
Tristan <tristanpang@users.noreply.github.com>
2023-10-27 11:31:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import GUI_file_importer
+from data_import import *
+from plotting.py import *
+
+def main():
+  # GUI import
+  collar_data, survey_data, sample_data, geology_data = GUI_file_importer.main()
+
+  # Data stuff
+  surveys = import_survey(survey_data)
+  collars = import_collar(collar_data, surveys)  
+  samples = import_sample(sample_data, collars)
+  tops = import_geology(geology_data, collars)
+
+  # Plotting
+  plot(collar_data, sample_data)
+  # Saving
+
+  return collars, surveys, samples, tops
+  
+
+if __name__ == "__main__":
+    main()
