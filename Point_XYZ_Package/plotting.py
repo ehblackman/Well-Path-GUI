764c126c0211a5f002c028c4b58b8556bc33dcad
Edward Blackman <ehblackman500@gmail.com>
2023-10-30 11:55:18 +0000

    fixed the plotting the script to have all wells plot at once

--- Point_XYZ_Package/plotting.py
+++ Point_XYZ_Package/plotting.py
@@ -1,25 +1,27 @@
-#from data_import import *
 import plotly.graph_objects as go
 
-#survey_data = import_survey(path, survey_csv)
-#collar_data = import_collar(path, collar_csv, survey_data)
-#sample_data = import_sample(path, sample_csv, collar_data)
-#geology_data = import_geology(path, geology_csv, collar_data)
-
 def plot(collar_data, sample_data):
-    for well in collar_data['well']:
-        fig = well.plot(names=['Test'])
-    
-    # Plot samples
-    data = sample_data[sample_data['UWI']=='test-well2']
-    x = data['Easting'] 
-    y = data['Northing']
-    z = data['TVD']
-    fig.add_trace(
-        go.Scatter3d(x=x, y=y, z=z, 
-                        marker=dict(color='red',size=10),
-                        line=dict(width=0),
-                        name=f'Sample'))
-    
-    # Show fig
+    # Create a single figure to hold all well plots and sample data
+    fig = go.Figure()
+
+    # Iterate through all wells
+    for uwi in collar_data['UWI'].unique():
+        data = collar_data[collar_data['UWI'] == uwi]
+        well_fig = data['well'].iloc[0].plot()  # Assuming "plot" method is used to visualize well
+        for trace in well_fig['data']:
+            fig.add_trace(trace)  # Add all traces of the well plot to the main figure
+
+    # Plot samples for all wells
+    for uwi in collar_data['UWI'].unique():
+        data = sample_data[sample_data['UWI'] == uwi]
+        x = data['Easting']
+        y = data['Northing']
+        z = data['TVD']
+        fig.add_trace(
+            go.Scatter3d(x=x, y=y, z=z,
+                         marker=dict(color='red', size=10),
+                         line=dict(width=0),
+                         name=f'Sample - {uwi}'))
+
+    # Show the combined figure with all well plots and sample data
     fig.show()
