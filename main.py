abef9262bb0d14bd634f1c6e07abc231adf65613
Edward Blackman <ehblackman500@gmail.com>
2023-10-30 11:27:56 +0000

    renaming and moving

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import Point_XYZ_Package as xyz
 
 def main():
-  # GUI import
+  # GUI data import
   collar_data, survey_data, sample_data, geology_data = xyz.main()
 
-  # Data stuff
+  # Data stuff, manipulation, calculations etc
   surveys = xyz.import_survey(survey_data)
   collars = xyz.import_collar(collar_data, surveys)  
   samples = xyz.import_sample(sample_data, collars)
